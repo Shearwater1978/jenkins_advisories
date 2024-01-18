9a94b41aaa2903af57628028bce1a541e355dffb
Uglykoyote <uglykoyote@yandex.ru>
2024-01-18 11:14:10 +0100

    Create rss_feed_reader.py

---
+++ rss_feed_reader.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import datetime
+import logging
+import re
+
+import feedparser
+
+RSS_FEED_URL = 'https://www.jenkins.io/security/advisories/rss.xml'
+HowDeepItemsLookBack = 1
+DATE_FORMAT_STR = '%a, %d %b %Y %I:%M:%S %z'
+SENSITIVE_PLUGINS = ['saml', 'kubernetes']
+
+loggeg = logging.getLogger(__name__)
+FORMAT_INFO = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=FORMAT_INFO, level=logging.INFO)
